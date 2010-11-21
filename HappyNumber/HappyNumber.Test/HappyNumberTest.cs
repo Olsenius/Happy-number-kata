ddbb09e66f5edc720ed2d85c1c3a07286abc9efd
Andreas Lien Olsen <andreas.olsen@gmail.com>
2010-11-21 16:23:10 +0100

    Added first failing test

---
+++ HappyNumber/HappyNumber.Test/HappyNumberTest.cs
@@ -0,0 +1,25 @@
+/*
+ * http://codingkata.org/katas/unit/happy-number
+ * http://en.wikipedia.org/wiki/Happy_number
+ * 
+ * A happy number is defined by the following process. Starting with any positive integer, 
+ * replace the number by the sum of the squares of its digits, and repeat the process until 
+ * the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not 
+ * include 1. Those numbers for which this process ends in 1 are happy numbers, while those 
+ * that do not end in 1 are unhappy numbers (or sad numbers).
+ * */
+
+using NUnit.Framework;
+
+namespace HappyNumber.Test
+{
+    [TestFixture]
+    public class HappyNumberTest
+    {
+       [Test]
+        public void One_is_happy()
+       {
+           1.IsHappy().ShouldBeTrue();
+       }
+    }
+}
